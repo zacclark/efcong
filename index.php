96e7df136d74eb5ba6633eda5bf20cb6b3589fca
jacob-etter <jacob.etter@colorado.edu>
2010-11-03 14:24:54 -0600

    Finished the htaccess file and updated the templates

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php require('templates/header.php'); ?>
-			<div id="imageBox"></div>
+			
+	<?php
+	$path = $_GET["path"];
+	?>
+
+	<?php if ($path == ""|| $path == "home") { ?>
 			
 			<div id="contentBox">
 				<h2>About the CNGEF</h2>
@@ -9,4 +14,38 @@
 					<p>The Education Foundation feels we can help these American heroes, by helping their families. Grants awarded by the Education Founda- tion are for Colorado National Guard members, and their immediate families only. These grants help our State’s citizen-soldiers by help- ing with the cost of higher education for their dependents, often while they are deployed for extended periods in very dangerous, remote locations. The dependents of 6,000 Colorado Air and Army National Guard families called to serve in the War-on-Terror, had little or no State or Federal education benefits available to them. The Foundation’s Mission is to provide counseling and tuition grants enabling dependents to begin or continue their college or technical school education. To date the Foundation has provided over $200,000 to 202 dependents.</p>
         		</div>
 			</div>
+	<?php }else if ($path == "news") { ?>
+			<div id="contentBox">
+				<h2>News Page (coming soon)</h2>	
+				<div class="inner">		
+				</div>
+			</div>
+	<?php }else if ($path == "scholarship") { ?>
+		<div id="contentBox">
+			<h2>Scholarship Page (coming soon) </h2>
+			<div class="inner">
+			</div>
+		</div>
+	<?php }else if ($path == "history") { ?>
+		<div id="contentBox">
+			<h2>History Page (coming soon) </h2>
+			<div class="inner">
+			</div>
+		</div>
+	<?php }else if ($path == "donate") { ?>
+		<div id="contentBox">
+			<h2>Donation Page (coming soon) </h2>
+			<div class="inner">
+			</div>
+		</div>
+	<?php }else if ($path == "contact") { ?>
+		<div id="contentBox">
+			<h2>Contact Page (coming soon) </h2>
+			<div class="inner">
+			</div>
+		</div>
+	<?php }else{ ?>
+		404 <?php echo $_GET["path"]; ?>
+	<?php } ?>
+	
 <?php require('templates/footer.php'); ?>
